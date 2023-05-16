deee2d5d65432341206a6c8d8c9fdbe410a3c099
JMW_1999 <54145971+Zero-coder@users.noreply.github.com>
2023-05-16 12:46:48 -0400

    Add files via upload

---
+++ scripts/ILI.sh
@@ -0,0 +1,17 @@
+pred_l=(24 36 48 60)
+
+for i in ${pred_l[@]}
+do
+    python -u run.py \
+        --model mlgn \
+        --mode regre \
+        --data ILI \
+        --features M \
+        --freq d \
+        --conv_kernel 18 12 \
+        --d_layers 1 \
+        --d_model 64 \
+        --seq_len 36 \
+        --label_len 36 \
+        --pred_len $i
+done
